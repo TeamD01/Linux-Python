265ce6f450feca60f702252c2c0e5e75d0b1b0e6
TeamD01 <david.monahan57@gmail.com>
2023-01-03 23:23:25 +0000

    challenge 18 initial

---
+++ lab_input/lab18.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def main():
+    user_name_in = input("Enter your name: ")
+    day_of_week_in = input("enter the day of the week: ")
+
+    print("Hello, " + user_name_in + "! Happy " + day_of_week_in + "!")
+
+main()
