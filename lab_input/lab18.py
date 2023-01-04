f08cefc1f1cb7fb3a2da47bdc58e293be7955852
TeamD01 <david.monahan57@gmail.com>
2023-01-04 16:13:41 +0000

    added comments

--- lab_input/lab18.py
+++ lab_input/lab18.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+'''This is Lab 18 print() and input() examples and run
+'''
 def main():
     user_name_in = input("Enter your name: ")
     day_of_week_in = input("enter the day of the week: ")
