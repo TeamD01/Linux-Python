17288aa4255dae539aa664820f6bb396f02835ad
TeamD01 <david.monahan57@gmail.com>
2023-01-05 03:22:57 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""OPTIONAL CHALLENGE: Create a list of at least five animals with three letter names and have your program output them to the screen. No need to be fancy, just load some animals in a list, and print them out, like the list below. If you get stuck, as the instructor for some help. When you finish. Save your code as /home/student/mycode/advlist/animal-list.py"""
+
+def main():
+    # create a list called list1
+    list1 = ["Fox", "Fly", "Ant", "Bee", "Cod", "Cat", "Dog", "Yak", "Cow", "Hen", "Koi", "Hog",  "Jay", " Kit"]
+
+    # display list1
+    print(list1)
+
+main()
+
