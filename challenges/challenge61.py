2c6214688e596806af171f32cc911449f01aee15
TeamD01 <david.monahan57@gmail.com>
2023-01-06 22:25:32 +0000

    Challenges 61 and 62

---
+++ challenges/challenge61.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def main():
+    string1 = " bottles of beer on the wall! "
+    string2 = " bottles of beer! "
+    string3 = " You take one down, pass it around!"
+
+    verses= int(input("Number of verses to sing (1 - 100)"))
+    if verses > 0 and verses <= 100:
+        for i in range(verses, 0, -1):
+            print(str(i) + string1 + "\n" + str(i) + string1 + str(i) + string2 + string3)
+    else:
+        print("invalid input ~~~ bye!!!")
+main()
